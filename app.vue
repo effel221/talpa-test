e2c823fb2d58f589941b2b9607dc8ea9476da418
Tatsiana-Salikayeva <tatsiana.salikayeva@promaterial.com>
2024-08-13 18:45:58 +0200

    create styles for each product

--- app.vue
+++ app.vue
@@ -3,13 +3,15 @@
 <template>
   <header class="mb-10">
     <div class="flex">
-      <div class="grid-cols-1 text-green-500 text-2xl m-4 font-bold">Travel Paradise Site</div>
+      <div class="grid-cols-1 text-green-500 text-2xl m-4 font-bold">
+        <a href="/">Travel Paradise Site</a>
+      </div>
     </div>
 
   </header>
   <main>
     <div class="flex flex-wrap justify-center">
-        <Product v-for="item in products?.products" :key="item.id" :item="item" />
+        <Product v-for="item in result?.products" :key="item.id" :item="item" />
     </div>
   </main>
 </template>
@@ -21,9 +23,6 @@ import type {ProductsInteface} from "./types_interfaces/interfaces";
 
 
 
-const { result: products } = useQuery<ProductsInteface>(getProducts)
-if ( products) {
-  console.log(products)
-}
+const { result } = useQuery<ProductsInteface>(getProducts)
 
 </script>
--- components/Product.vue
+++ components/Product.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import moment from "moment";
 import type {Product} from "../types_interfaces/interfaces"
 interface Props  {
   item: Product
@@ -11,14 +12,26 @@ const isCar = ref(props.item.type === "car_rentals")
 </script>
 
 <template>
-  <div class="w-72 m-1 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+  <div class="relative w-72 m-1 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+    <span
+      class="absolute	px-1 right-1 top-1 text-white rounded-lg text-sm"
+      :class="{
+        'bg-fuchsia-800': isFlight,
+        'bg-orange-500': isCar,
+        'bg-sky-700': isHotel}"
+    >
+      <span v-show="isFlight">Flight</span>
+      <span v-show="isHotel">Hotel</span>
+      <span v-show="isCar">Car</span>
+    </span>
     <h5
         class="mb-2 text-2xl font-bold tracking-tight"
         :class="{
         'text-fuchsia-800': isFlight,
         'text-orange-500': isCar,
         'text-sky-700': isHotel}">{{item.name}}</h5>
-    <p class="font-bold text-cyan-950">{{item.city}}</p>
+    <p v-show="isFlight"><strong>Flight date:</strong> {{moment(item.date).format("MMM Do YY")}}</p>
+    <p class="text-cyan-950"><strong>City:</strong> {{item.city}}</p>
   </div>
 </template>
 
